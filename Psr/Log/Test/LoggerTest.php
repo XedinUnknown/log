7ca7736ea29c0b2c9455fc567d4a14bc8f3f3f0e
Anton Ukhanev <xedin.unknown@gmail.com>
2021-06-06 12:56:29 +0200

    Add logger test

---
+++ Psr/Log/Test/LoggerTest.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace Psr\Log\Test;
+
+
+use PHPUnit\Framework\TestCase;
+use Psr\Log\LogLevel;
+
+class LoggerTest extends TestCase
+{
+    /**
+     * @return \PHPUnit\Framework\MockObject\MockObject|TestLogger
+     */
+    protected function createSubject()
+    {
+        $mock = $this->getMockBuilder('Psr\Log\Test\TestLogger')
+            ->addMethods(array())
+            ->enableProxyingToOriginalMethods()
+            ->getMock();
+
+        return $mock;
+    }
+
+    public function testLog()
+    {
+        $message = uniqid('message');
+        $level = LogLevel::INFO;
+        $subject = $this->createSubject();
+
+        $subject->log($level, $message, array());
+
+        $this->assertTrue($subject->hasRecord($message, $level));
+    }
+}
